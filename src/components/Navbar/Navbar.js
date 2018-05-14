5f5f7f2d5dc6356f7a25258867917097af86b5db
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-13 22:11:11 -0400

    router fix v2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/" rel="noopener noreferrer">Daniel Doyle</a>
+        <a className="navbar-brand" href="/Bootstrap-Portfolio/" rel="noopener noreferrer">Daniel Doyle</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -11,7 +11,7 @@ const Navbar = () => (
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav justify-content-end">
             <li className="nav-item active">
-                <a className="nav-link" href="/" rel="noopener noreferrer">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/Bootstrap-Portfolio/" rel="noopener noreferrer">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
                 <a className="nav-link" href="/portfolio" rel="noopener noreferrer">Portfolio</a>
