ce6bfd7eff921ef08178386e186247d3e120a632
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-29 16:09:42 -0400

    started hovering state

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -8,8 +8,17 @@ class Carousel extends Component {
         super(props)
 
         this.state = {
-
+            toggled: false
         }
+
+        this.onMouseEvent = this.onMouseEvent.bind(this)
+    }
+
+    onMouseEvent () {
+        this.setState({
+            toggled: !this.state.toggled
+        })
+        console.log(this.state.toggled)
     }
 
 
@@ -20,7 +29,7 @@ class Carousel extends Component {
                 <CardMedia
                 overlay={<CardTitle title={this.props.name} subtitle={"content"}/>}
                 >
-                <img src={this.props.gif} alt={this.props.name}/>
+                <img src={this.props.gif} alt={this.props.name} onMouseEnter={this.onMouseEvent} onMouseLeave={this.onMouseEvent}/>
                 </CardMedia>
                 <a href={this.props.url} target='_blank' rel="noopener noreferrer"><button className='btn carouselBtn btn-secondary'>{this.props.name}</button></a>
                 <a href={this.props.github} target='_blank' rel="noopener noreferrer"><button className='btn carouselBtn btn-secondary'>{this.props.name} Github</button></a>
