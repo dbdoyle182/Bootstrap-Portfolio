ea03eca435e6743a7caa4e35f4ca10632e3104d8
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-29 16:05:25 -0400

    starting mouse over

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardMedia, CardTitle } from 'material-ui/Card';
 import './Carousel.css';
 
 
-const Carousel = props => (
+class Carousel extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
 
 
-    <section className='container'>
-        <Card>   
-            <CardMedia
-            overlay={<CardTitle title={props.name} />}
-            >
-            <img src={props.gif} alt={props.name}/>
-            </CardMedia>
-            <a href={props.url} target='_blank' rel="noopener noreferrer"><button className='btn carouselBtn btn-secondary'>{props.name}</button></a>
-            <a href={props.github} target='_blank' rel="noopener noreferrer"><button className='btn carouselBtn btn-secondary'>{props.name} Github</button></a>
-        </Card>
-    </section>
-);
+    render() {
+        return (
+        <section className='container'>
+            <Card>   
+                <CardMedia
+                overlay={<CardTitle title={this.props.name} subtitle={"content"}/>}
+                >
+                <img src={this.props.gif} alt={this.props.name}/>
+                </CardMedia>
+                <a href={this.props.url} target='_blank' rel="noopener noreferrer"><button className='btn carouselBtn btn-secondary'>{this.props.name}</button></a>
+                <a href={this.props.github} target='_blank' rel="noopener noreferrer"><button className='btn carouselBtn btn-secondary'>{this.props.name} Github</button></a>
+            </Card>
+        </section>
+        )
+    }
+};
 
 export default Carousel;
