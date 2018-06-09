4b2b3b0a0dd659817ad4067d775376c9aa91365e
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-08 23:40:40 -0400

    removed unnecessary buttons and changed sidenav

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -89,31 +89,9 @@ class Portfolio extends Component {
 
                 </div>
                 <br />
-                <div className='row'>
-                    <div className='col-md-12'>
-                        <div className='container'>
-                            <div className='row'>
-                                <div className='btnContainer col-sm-6 col-md-6'>
-                                    <button onClick={this.decrementClick.bind(this)} className='btn btnPosition btn-secondary'>
-                                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span className="sr-only">Previous</span>
-                                    </button>
-                                </div>
-                                <div className='btnContainer col-sm-6 col-md-6'>
-                                    <button onClick={this.incrementClick.bind(this)} className='btn btnPosition btn-secondary'>
-                                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span className="sr-only">Next</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <br /><br />
-
+                          
                 
-                
-                
+               
             </section>          
         )
     }
